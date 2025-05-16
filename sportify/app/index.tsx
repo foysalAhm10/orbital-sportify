71a8d918a847444cdae93814a37b152b89d1ec8e
antorc <antorchowdh@gmail.com>
2025-05-16 12:11:43 +0800

    changed index.tsx

--- sportify/app/index.tsx
+++ sportify/app/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center">
-      <Text className="text-3xl text-blue-500 font-bold">Welcome to Sportify!</Text>
+      <Text className="text-3xl text-blue-500 font-bold">WELCOME!!!</Text>
     </View>
   );
 }
